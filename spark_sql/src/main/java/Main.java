b1cdebff5665d357fbf3d3afe129af3723e2e479
张亚中 <ZhongCentury@gmail.com>
2016-11-21 20:29:58 +0800

    SparkSQL Demo

---
+++ spark_sql/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.sql.Timestamp;
+
+/**
+ * @author zhangyazhong
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        Timestamp timestamp = new Timestamp(1479722879000L);
+        System.out.println(timestamp);
+    }
+}
---
+++ spark_sql/src/main/scala/SqlTest.scala
@@ -0,0 +1,55 @@
+import org.apache.spark
+import org.apache.spark.sql.SparkSession
+
+object SqlTest {
+
+    case class Person(name: String, age: Int, gender: String)
+
+    def plus(x: Int, y: Int) : Int = {
+        val z: Int = x + y
+        z
+    }
+
+    def minus(x : Int, y: Int) : Int = plus(x, y)
+
+    def main(args: Array[String]) {
+
+
+        val sparkSession = SparkSession
+            .builder()
+            .master("local")
+            .appName("SparkDemo")
+            .getOrCreate()
+
+
+        import sparkSession.implicits._
+        /*
+        val df = sparkSession.read.json("spark_sql/src/main/resources/persons.json")
+        df.createOrReplaceTempView("person")
+        val sqlDF = sparkSession.sql("SELECT * FROM person WHERE age<20")
+        sqlDF.show()
+        */
+        val personDataFrame = sparkSession.sparkContext.textFile("spark_sql/src/main/resources/persons.txt")
+            .map(_.split(","))
+            .map(cols => Person(cols(0), cols(1).trim.toInt, cols(2)))
+            .toDF()
+
+        personDataFrame.createOrReplaceTempView("person")
+        val teenagersDF = sparkSession.sql("SELECT name, age, gender FROM person WHERE age BETWEEN 13 AND 19")
+        teenagersDF.show()
+
+
+//        df.createOrReplaceTempView("person")
+//        val all = spark.sql("select * from person")
+//        println(all)
+//        df.show()
+//        df.show()
+//        val conf = new SparkConf().setMaster("local").setAppName("demo")
+//        val sc = new SparkContext(conf)
+//        val sqlContext = new SQLContext(sc)
+//        val persons = sc.textFile("examples/src/main/resources/persons.txt").map(_.split(",")).map(p => Person(p(0), p(1).trim.toInt)).toDF()
+//        persons.registerAsTable("person")
+//        val teenagers = sqlContext.sql("SELECT name FROM person WHERE age >= 13 AND age <= 19")
+//        teenagers.map(t => "Name: " + t(0)).collect().foreach(println)
+    }
+}
